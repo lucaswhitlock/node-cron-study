2c1c31bcedcd15d1c62c2f17b348ea800b74442b
lwhitlock <lwhitlock@fazenda.ms.gov.br>
2018-09-05 16:22:49 -0400

    Reutilizando service

--- app/controllers/scheduler-actions-controller.js
+++ app/controllers/scheduler-actions-controller.js
@@ -1,42 +1,11 @@
 import cron from 'cron';
-import mongoose from 'mongoose';
-import moment from 'moment';
-import dbconf from '../etc/database-config';
-import fatoObservado from '../models/fo-schema';
-import foStatus from '../models/status-fo-enum';
+import * as jobService from '../services/job-service';
 
 var job = new cron.CronJob({
     cronTime: '*/30 * * * * *',
-    onTick: async () => {
-            console.log('Tentando realizar a conexão na base de dados...');
-            await mongoose.connect(dbconf.URL_LOCAL).then(() => {
-                console.log('Conectado.');
-            }).catch((err) => {
-                console.log('Erro: ' + err);
-            });
-            console.log('Buscando FOs respondidos...')
-            let fo = await fatoObservado.find({
-                foStatus: foStatus.ABERTO
-            }).then((fos) => {
-                console.log('[' + fos.length + '] FOs encontrados.');
-                console.log('Iniciando atualizacao dos FOs...');
-                fos.forEach((fo) => {
-                    console.log('Atualizando FO com id: [' + fo._id + ']');
-                    console.log('Diferença de dias até hoje: ' + diffDays(fo.createdAt));
-                    if (diffDays(fo.createdAt) > 2) {
-                        fatoObservado.findOneAndUpdate({ foStatus: foStatus.EXPIRADO }, { _id: fo._id });
-                    }
-                    console.log("FO atualizado: " + fo);
-                });
-            }).catch((err) => {
-                console.log(err);
-            });
-            console.log('Atualizações realizadas com sucesso. \nFinalizando processo.');
-            mongoose.disconnect();
-            console.log('Processo finalizado.');
-        },
-        start: true,
-        runOnInit: true
+    onTick: jobService.execute(),
+    start: true,
+    runOnInit: true
 });
 
 exports.start = (req, res) => {
@@ -78,8 +47,3 @@ exports.refresh = (req, res) => {
         }
     });
 };
-
-function diffDays(day) {
-    var today = moment(Date.now());
-    return today.diff(moment(day), 'days');
-}
--- app/services/job-service.js
+++ app/services/job-service.js
@@ -1,28 +1,45 @@
 import mongoose from 'mongoose';
+import moment from 'moment';
 import dbconf from '../etc/database-config';
 import fatoObservado from '../models/fo-schema';
 import foStatus from '../models/status-fo-enum';
 
-var execute = async function () {
+const execute = async () => {
     console.log('Tentando realizar a conexão na base de dados...');
-    mongoose.connect(dbconf.URL_LOCAL, {
-        useNewUrlParser: true
-    }).then(() => {
+    await mongoose.connect(dbconf.URL_LOCAL).then(() => {
         console.log('Conectado.');
     }).catch((err) => {
         console.log('Erro: ' + err);
     });
     console.log('Buscando FOs respondidos...')
     let fo = await fatoObservado.find({
-        foStatus: foStatus.RESPONDIDO
-    }).then((fo) => {
-        console.log('FO encontrado: ' + fo);
+        foStatus: foStatus.ABERTO
+    }).then((fos) => {
+        console.log('[' + fos.length + '] FOs encontrados.');
+        console.log('Iniciando atualizacao dos FOs...');
+        fos.forEach((fo) => {
+            console.log('Atualizando FO com id: [' + fo._id + ']');
+            console.log('Diferença de dias até hoje: ' + diffDays(fo.createdAt));
+            if (diffDays(fo.createdAt) > 2) {
+                fatoObservado.findOneAndUpdate({
+                    foStatus: foStatus.EXPIRADO
+                }, {
+                    _id: fo._id
+                });
+            }
+            console.log("FO atualizado: " + fo);
+        });
     }).catch((err) => {
         console.log(err);
     });
-    console.log('Desconectando da base de dados...');
+    console.log('Atualizações realizadas com sucesso. \nFinalizando processo.');
     mongoose.disconnect();
-    console.log('Desconectado');
+    console.log('Processo finalizado.');
 }
 
-export default execute;
+function diffDays(day) {
+    var today = moment(Date.now());
+    return today.diff(moment(day), 'days');
+}
+
+export { execute };
